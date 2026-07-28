34313763e407dc791c4f71c90ca0f93c1aaf9124
KennethJanLopez <lkennethjan@gmail.com>
2026-07-29 01:38:19 +0800

    chore: remove scratch scripts, old test files, and zip artifact

--- apps/admin-api/check.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const admins = await prisma.superAdmin.findMany();
-  console.log(admins);
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
